000c9bc82a0b7947a93375cca3d143d42aa91744
Jhones Bazilio dos Santos <jhones.santos@ituran.com.br>
2019-10-16 20:21:12 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { StepsComponent } from './steps/steps.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StepsComponent
   ],
   imports: [
     BrowserModule,
