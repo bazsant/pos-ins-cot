9c156fd8836551eea4f2c08b22dcbe2403ad59fa
Gustavo Gilberto Godinho <gustavo.godinho@ituran.com.br>
2019-10-23 18:18:12 -0300

    inclusao do component meu plano  e json prices para o combox quanto ela custa

---
+++ src/app/steps/meu-plano/meu-plano.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-plano',
+  templateUrl: './meu-plano.component.html',
+  styleUrls: ['./meu-plano.component.scss']
+})
+export class MeuPlanoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
